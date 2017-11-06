d9f89bb5f553fc1cd35aedf928f83b9470a80fab
Dhananjay P Shinkar <dps48@afsconnect1.njit.edu>
2017-11-06 01:04:31 -0500

    using generateHTML function

--- index.php
+++ index.php
@@ -63,16 +63,38 @@ class Database
     			
 	}
 
-
-	
+/*public function displayTable()
+{
+     
+      echo "<table border=2>";
+      echo "<tr><th>id</th><th>email</th><th>fname</th><th>lname</th><th>phone</th><th>birthday</th><th>gender</th><th>password</th></tr>";
+ 
+    foreach( $resultset as $row) {
+      echo "<tr>";
+      echo "<td>".$row['id']."</td>";
+      echo "<td>".$row['email']."</td>";
+      echo "<td>".$row['fname']."</td>";
+      echo "<td>".$row['lname']."</td>";
+      echo "<td>".$row['phone']."</td>";
+      echo "<td>".$row['birthday']."</td>";
+      echo "<td>".$row['gender']."</td>";
+      echo "<td>".$row['password']."</td>";
+      echo "</tr>";
+  }
+  
+  print_r($resultset);
+ 
+echo "</table>";
+    
+    }
+*/
 }
-
-
-
 
 $count=$db->getCount();
 print_r($count);
 $result=$db->getRows();
+//$db->displayTable();
+
     //print_r($result);
 
 
@@ -80,7 +102,8 @@ echo "<table border=1>";
 echo "<tr><th>ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone</th><th>Birth Date</th><th>Gender</th><th>Password</th></tr>";
 
 
-foreach( $result as $row) {
+foreach( $result as $row) 
+{
     echo "<tr>";
     echo "<td>".$row['id']."</td>";
     echo "<td>".$row['email']."</td>";
